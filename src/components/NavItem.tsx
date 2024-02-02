c820d8c64694adfc3ef882e793f86e8aa267810b
Fabio Viscuso <viscuso.fabio@outlook.it>
2024-02-02 22:25:17 +0100

    fix navItem

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,62 +1,99 @@
-"use client"
+'use client'
 
-import { ChevronDown } from "lucide-react"
-import { Button } from "./ui/button"
-import type { Category } from "@/config"
-import { cn } from "@/lib/utils"
-import Image from "next/image"
-import Link from "next/link"
+import { PRODUCT_CATEGORIES } from '@/config'
+import { Button } from './ui/button'
+import { ChevronDown } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
 
-interface Props {
-    category: Category,
-    handleOpen: () => void,
-    isOpen: boolean,
+type Category = (typeof PRODUCT_CATEGORIES)[number]
+
+interface NavItemProps {
+    category: Category
+    handleOpen: () => void
+    close: () => void
+    isOpen: boolean
     isAnyOpen: boolean
 }
 
-const NavItem = ({ isAnyOpen, isOpen, handleOpen, category }: Props) => {
+const NavItem = ({
+    isAnyOpen,
+    category,
+    handleOpen,
+    close,
+    isOpen,
+}: NavItemProps) => {
     return (
-        <div className="flex">
-            <div className="relative flex">
+        <div className='flex'>
+            <div className='relative flex items-center'>
                 <Button
+                    className='gap-1.5'
                     onClick={handleOpen}
-                    className="gap-1.5"
-                    variant={isOpen ? "secondary" : "ghost"}
-                >
+                    variant={isOpen ? 'secondary' : 'ghost'}>
                     {category.label}
-                    <ChevronDown className={cn("h-4 w-4 transition-all text-muted-foreground", { '-rotate-180': isOpen })} />
+                    <ChevronDown
+                        className={cn(
+                            'h-4 w-4 transition-all text-muted-foreground',
+                            {
+                                '-rotate-180': isOpen,
+                            }
+                        )}
+                    />
                 </Button>
+            </div>
 
-                {
-                    isOpen &&
-                    <div className={cn("absolute inset-x-0 top-full text-sm text-muted-foreground", { "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen })}>
-                        <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden />
-                        <div className="relative bg-white">
-                            <div className="mx-auto max-w-7 px-8">
-                                <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
-                                    <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
-                                        {category.featured.map(featured => (
-                                            <div key={featured.name} className="group relative sm:text-sm text-base">
-                                                <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                                    <Image
-                                                        src={featured.imgSrc}
-                                                        alt={`${featured.name} category image`}
-                                                        fill
-                                                        className="object-cover object-center"
-                                                    />
-                                                </div>
-                                                <Link href={featured.href} className="mt-6 block font-medium text-gray-900">{featured.name}</Link>
-                                                <p className="mt-1" aria-hidden >Shop now</p>
+            {isOpen ? (
+                <div
+                    onClick={() => close()}
+                    className={cn(
+                        'absolute inset-x-0 top-full text-sm text-muted-foreground',
+                        {
+                            'animate-in fade-in-10 slide-in-from-top-5':
+                                !isAnyOpen,
+                        }
+                    )}>
+                    <div
+                        className='absolute inset-0 top-1/2 bg-white shadow'
+                        aria-hidden='true'
+                    />
+
+                    <div className='relative bg-white'>
+                        <div className='mx-auto max-w-7xl px-8'>
+                            <div className='grid grid-cols-4 gap-x-8 gap-y-10 py-16'>
+                                <div className='col-span-4 col-start-1 grid grid-cols-3 gap-x-8'>
+                                    {category.featured.map((item) => (
+                                        <div
+                                            onClick={() => close}
+                                            key={item.name}
+                                            className='group relative text-base sm:text-sm'>
+                                            <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
+                                                <Image
+                                                    src={item.imgSrc}
+                                                    alt='product category image'
+                                                    fill
+                                                    className='object-cover object-center'
+                                                />
                                             </div>
-                                        )
-                                        )}
-                                    </div>
+
+                                            <Link
+                                                href={item.href}
+                                                className='mt-6 block font-medium text-gray-900'>
+                                                {item.name}
+                                            </Link>
+                                            <p
+                                                className='mt-1'
+                                                aria-hidden='true'>
+                                                Shop now
+                                            </p>
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                         </div>
                     </div>
-                }
-            </div>
+                </div>
+            ) : null}
         </div>
     )
 }
